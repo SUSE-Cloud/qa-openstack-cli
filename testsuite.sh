0fda187ec50679f388d194d0bbfe0aa64504dbbd
Dominik Heidler <dheidler@suse.de>
2013-02-14 11:00:32 +0000

    initial commit

---
+++ testsuite.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source config
+
+function printlog() {
+	echo "$1" | while read line ; do
+		echo "$line" | grep -q "^++" && continue
+		echo "  $line"
+	done
+	echo
+}
+
+function runtest() {
+	echo -n "Running $1 ... "
+	testlog="`./tests/$1 2>&1`"
+	res=$?
+
+	if [ "$res" == "0" ] ; then
+		echo -e "\e[00;32mOK\e[00m"
+		if [ "$DEBUG" == "1" ] ; then
+			printlog "$testlog"
+		fi
+	elif [ "$res" == "1" ] ; then
+		echo -e "\e[00;31mFAIL\e[00m"
+		printlog "$testlog"
+		return 1
+	elif [ "$res" == "2" ] ; then
+		echo -e "\e[1;31mERROR\e[00m"
+		printlog "$testlog"
+		return 1
+	elif [ "$res" == "3" ] ; then
+		echo -e "\e[1;33mSKIPPED\e[00m"
+	else
+		echo -e "\e[1;34mWTF?\e[00m"
+		printlog "$testlog"
+		return 1
+	fi
+	return 0
+}
+
+
+ls ./tests | while read testname ; do
+	runtest "$testname"
+done
